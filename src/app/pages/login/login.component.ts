043ad1581ae913bb137ee19151b23ea128dd16cd
Pedro Fernandez <pefernandez1408@gmail.com>
2024-05-14 19:09:27 -0400

    ADD: login service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  user: string;
+  password: string;
+
+  constructor() {
+    this.user = ''
+    this.password = ''
+  }
+
+  onLogin(){
+    console.log(this.user)
+    console.log(this.password)
+  }
 }

a0ca8c735515717490b519923a7a15ba41281b24
Pedro Fernandez <pefernandez1408@gmail.com>
2024-05-15 01:27:34 -0400

    ADD: form validations

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from '../../core/services/login.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
@@ -7,25 +7,37 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  
-  user: string;
-  password: string;
+  myForm!: FormGroup;
 
-  constructor(private loginService: LoginService, private router: Router) {
-    this.user = ''
-    this.password = ''
+  constructor(
+    private loginService: LoginService, 
+    private router: Router, 
+    private formBuilder: FormBuilder
+  ) {
+    this.myForm = this.formBuilder.group({
+      user: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    })
   }
 
-  onLogin(){
-    this.loginService.login({userName: this.user, password: this.password}).subscribe(res => console.log(res))
+  onSubmit() {
+    if (this.myForm.valid) {
+      const user = this.myForm.get('user')?.value;
+      const password = this.myForm.get('password')?.value;
+      this.loginService.login({userName: user, password: password}).subscribe(res => console.log(res))
+    }
+  }
+
+  // onLogin(){
+  //   this.loginService.login({userName: this.user, password: this.password}).subscribe(res => console.log(res))
     
-  }
+  // }
 
   signUpRedirect() {
     this.router.navigateByUrl('/sign-up')
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule, FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
-  constructor(private router: Router) {}
+  myForm!: FormGroup;
+
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      user: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      repeat_password: ['']
+    })
+  }
+
+  validatePassword(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmPassword = formGroup.get('repeat_password')?.value;
+
+    if (password !== confirmPassword) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      const user = this.myForm.get('user')?.value;
+      const email = this.myForm.get('email')?.value;
+      const password = this.myForm.get('password')?.value;
+      console.log(user)
+      console.log(email)
+      console.log(password)
+    }
+  }
 
   signInRedirect() {
     this.router.navigateByUrl('')
